5286aaf1adcd38d1a05ece99fa07d6048fb2536a
Pepe-studentu <155471233+Pepe-studentu@users.noreply.github.com>
2024-01-04 11:13:32 +0200

    Update supervisor_controller.c

--- supervisor_controller.c
+++ supervisor_controller.c
@@ -1,11 +1,16 @@
 #include <webots/robot.h>
 #include <webots/supervisor.h>
 #include <stdio.h>
+#include "maze.h"
+#include "structures.h"
 
 #define TIME_STEP 32
 
-// int** maze(int number_of_rows, int number_of_collumns) functie care genereaza matricea
-void spawn_maze(int **matrix)
+char position_string[100];
+int width = 10, height = 10, seed = 17;
+float normalizer = 0.2;
+
+void spawn_maze(int **matrix, int rows, int collumns)
 {
 
     for (int i = 0; i < rows; i++)
@@ -22,14 +27,14 @@ void spawn_maze(int **matrix)
 int main(int argc, char **argv)
 {
     wb_robot_init();
+    
+    srand(seed);
+    int **maze = generate_maze(&width, &height);
 
     WbNodeRef root_node = wb_supervisor_node_get_root();
     WbFieldRef children_field = wb_supervisor_node_get_field(root_node, "children");
-
-    char position_string[100];
-    float normalizer = 0.2; // the size of the wall
-    // scanf pt rows si collumns??
-    // spawn_maze(maze(rows, collumns);
+    
+    spawn_maze(maze, 2 * width + 1, 2 * height + 1);
 
     while (wb_robot_step(TIME_STEP) != -1)
     {
